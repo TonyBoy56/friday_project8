4049214557a13b7d8c9735125967bdb6f0a75146
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-03-07 14:04:35 -0800

    add functionality for user interface logic. The program works!

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,7 +7,19 @@ namespace WordCounter
   {
     public static void Main()
     {
-      Console.WriteLine("");
+      InitializeProgram();
+    }
+
+    public static void InitializeProgram()
+    {
+      Console.WriteLine("Hello! Please enter a sentence. It can be as long as you may like: ");
+      string inputtedSentence = Console.ReadLine();
+      Console.WriteLine("Enter a word that I can use to check the sentence for: ");
+      string inputtedWord = Console.ReadLine();
+
+      RepeatCounter newCounter = new RepeatCounter();
+      int iThinkThisWillWork = newCounter.TrackIterable(inputtedWord, inputtedSentence);
+      Console.WriteLine(inputtedWord + " occurs " + iThinkThisWillWork + " times in the sentence you have typed.");
     }
   }
 }
